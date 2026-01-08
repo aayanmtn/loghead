b9b2015f7d3a4600c55039b7784ce60fc7c59692
Ronnel Davis <ronneldavis1996@gmail.com>
2026-01-08 13:59:58 -0500

    Cleaned folder structure

--- packages/vs-code-extension/loghead/out/state/serverState.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverState = void 0;
-exports.serverState = {
-    running: false,
-    port: undefined,
-    mcpToken: undefined,
-};
-//# sourceMappingURL=serverState.js.map
