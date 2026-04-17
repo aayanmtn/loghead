b84acb2b5dbe38fbb1a405903d65062b8eb1d957
Ronnel Davis <ronneldavis1996@gmail.com>
2026-04-17 14:35:45 -0400

    Add cloud app favicon metadata

--- packages/app/app/(app)/layout.tsx
+++ packages/app/app/(app)/layout.tsx
@@ -1,11 +1,15 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Loghead",
   description: "Unified Logging, Simplified.",
+  icons: {
+    icon: "/logo.svg",
+  },
 };
 
 export default function AppLayout({
